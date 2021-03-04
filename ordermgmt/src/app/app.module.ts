5ac22845b419c63d6c2ecc5b8c99ed731a0cadbf
KT1999 <tejaakarapu@gmail.com>
2021-03-04 13:04:58 +0530

    ordermgmt

---
+++ ordermgmt/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTabsModule } from '@angular/material/tabs';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import { CustomersComponent } from './customers/customers.component';
+import {MatTableModule} from '@angular/material/table';
+import { OrdersComponent } from './orders/orders.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CustomersComponent,
+    OrdersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatSelectModule
+ 
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ordermgmt/src/app/orders/orders.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.css']
+})
+export class OrdersComponent implements OnInit {
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  } 
+}
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
